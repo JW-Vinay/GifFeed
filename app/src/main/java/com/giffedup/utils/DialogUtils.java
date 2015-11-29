bf51018ea65c087c064eea9910fc92057f6516b1
JW-Vinay <vinay.ramkrishnan@gmail.com>
2015-11-28 20:24:48 -0800

    added icons and listener files missed in previous commit

---
+++ app/src/main/java/com/giffedup/utils/DialogUtils.java
@@ -0,0 +1,75 @@
+package com.giffedup.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+
+import com.giffedup.R;
+
+/**
+ * Created by vinayr on 11/28/15.
+ */
+public class DialogUtils {
+
+    public static AlertDialog showDialog(Context context, int title) {
+        return showDialog(context, title, -1);
+    }
+
+    public static AlertDialog showDialog(Context context, int title, int message) {
+        return showDialog(context, title, message, R.string.btn_ok, null);
+    }
+
+    public static AlertDialog showDialog(Context context, int title, int message, int positiveBtn, final DialogClickListener dialogClickListener) {
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialogClickListener != null) {
+                    if (which == DialogInterface.BUTTON_POSITIVE) {
+                        dialog.dismiss();
+                        dialogClickListener.onPositiveBtnClick();
+                    }
+                    else if (which == DialogInterface.BUTTON_NEGATIVE) {
+                        dialog.dismiss();
+                        dialogClickListener.onNegativeBtnClick();
+                    }
+
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.compatDialogStyle);
+        builder.setTitle(title);
+        if (message != -1) ;
+        builder.setMessage(message);
+        builder.setPositiveButton(positiveBtn, listener);
+        builder.setNegativeButton(R.string.btn_cancel, listener);
+        return builder.show();
+    }
+
+    public static AlertDialog showDialog(Context context, String title, String message, int positiveBtn, final DialogClickListener dialogClickListener) {
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialogClickListener != null) {
+                    if (which == DialogInterface.BUTTON_POSITIVE){
+                        dialog.dismiss();
+                        dialogClickListener.onPositiveBtnClick();
+                    }
+                    else if (which == DialogInterface.BUTTON_NEGATIVE) {
+                        dialog.dismiss();
+                        dialogClickListener.onNegativeBtnClick();
+                    }
+
+                }
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.compatDialogStyle);
+        builder.setTitle(title);
+        if (!TextUtils.isEmpty(message)) ;
+        builder.setMessage(message);
+        builder.setPositiveButton(positiveBtn, listener);
+        builder.setNegativeButton(R.string.btn_cancel, listener);
+        return builder.show();
+    }
+}
